723af0286469632eaf806c31b6f81baa12358e1a
Mat-Shiro98 <matias_shiroma@hotmail.com>
2023-12-11 21:23:27 -0300

    Trabajo UTN ver.12

---
+++ clase 8/test1.js
@@ -0,0 +1,42 @@
+/*
+let nombre_apellido = "Matt";
+let numero = 25;
+let boolean = false;
+
+console.log(nombre_apellido+ " " + numero+ " " + boolean)
+*/
+
+
+/*
+// Array
+let fruta = ['manzanas', 'peras', 'frutilla']
+console.log(fruta[2]);
+
+
+//Diccionario o objeto
+
+const persona = {
+    nombre:'Juan',
+    apellido:'Perez',
+    edad: 32
+}
+
+console.log(persona.nombre);
+*/
+
+//Array + objeto
+
+const autos = [
+//Objeto 1    
+    {
+        marca:'toyota',
+        modelo: 2004
+    },
+//Objeto 2    
+    {
+        marca:'Audi',
+        modelo:'2015'
+    }
+]
+
+console.log(autos[0].marca);
